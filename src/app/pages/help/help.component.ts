3dfac83dca1b0830900d1dafdbae7c1fa1124fac
robert-upwork <robyop.eyeco@gmail.com>
2021-01-10 04:04:21 +0700

    fix all bugs

--- src/app/pages/help/help.component.ts
+++ src/app/pages/help/help.component.ts
@@ -2,7 +2,8 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'rtms-help',
-  templateUrl: './help.component.html'
+  templateUrl: './help.component.html',
+  styleUrls: ['./help.component.scss']
 })
 export class HelpComponent implements OnInit {
 
